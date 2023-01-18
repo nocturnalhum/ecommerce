9f6ef23f41395455969917130ef83a3f7a302f81
nocturnal_hum <thanin.chan@gmail.com>
2023-01-18 13:22:24 -0500

    Debug: Box Shadow Inset Issue With Mobile Devices

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -11,8 +11,8 @@ export default function Contact() {
   return (
     <>
       <Layout title='Contact'>
-        <div className='flex justify-center bg-[#ecf0f3] h-[950px] relative z-[-2]'>
-          <div className='container rounded-xl p-10 pb-20 bg-[#ecf0f3] shadow-form top-20 absolute z-[-1]'>
+        <div className='flex justify-center bg-[#ecf0f3] h-[950px] relative'>
+          <div className='container rounded-xl p-10 pb-20 bg-[#ecf0f3] shadow-form top-20 absolute z-[1]'>
             <h1 className='text-3xl font-bold'>Contact</h1>
             <form
               action=''
@@ -22,13 +22,13 @@ export default function Contact() {
                 type='text'
                 name='name'
                 placeholder='Name'
-                className='w-full h-10 pl-6 my-4 border-none outline-none rounded-lg bg-[#ecf0f3] shadow-finput focus:shadow-ffocus'
+                className='w-full h-10 pl-6 my-4 rounded-lg bg-[#ecf0f3] shadow-finput focus:shadow-ffocus'
               />
               <input
                 type='email'
                 name='email'
                 placeholder='Email'
-                className='w-full h-10 pl-6 my-4 border-none outline-none rounded-lg bg-[#ecf0f3] shadow-input focus:shadow-focus'
+                className='w-full h-10 pl-6 my-4 rounded-lg bg-[#ecf0f3] shadow-inner'
               />
               <input
                 type='text'
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         form: '10px 10px 10px rgba(209, 217, 230, 1.0), -10px -10px 10px rgba(255,255,255, 1.0)',
         finput: 'inset 2px 2px 4px rgba(209, 217, 230, 1.0)',
         ffocus:
-          '4px 4px 4px rgba(209, 217, 230, 1.0) inset, -4px -4px 16px rgba(255,255,255, 1.0) inset',
+          'inset 4px 4px 4px rgba(209, 217, 230, 1.0) inset, inset -4px -4px 16px rgba(255,255,255, 1.0)',
       },
     },
   },
