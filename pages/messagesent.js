591f624a4e4b1ba8eee9e804dcec41e032cac40c
nocturnal_hum <thanin.chan@gmail.com>
2023-01-23 19:45:59 -0500

    Implemented Message Sent Success Form

--- pages/messagesent.js
+++ pages/messagesent.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import Layout from '../components/Layout';
 
-export default function messagesent() {
-  return <div>messagesent</div>;
+export default function MessageSent() {
+  const router = useRouter();
+  return (
+    <Layout text='Success'>
+      <main className='h-screen flex flex-col bg-offwhite1  items-center  justify-start  '>
+        <div className='max-w-md font-bold bg-offwhite1 mx-5 rounded-lg text-gray-700 p-6 mt-40 shadow-form'>
+          <h1>Message has been sent from {router.query.email}.</h1>
+          <h1>We'll contact you shortly {router.query.name}✨</h1>
+        </div>
+      </main>
+    </Layout>
+  );
 }
